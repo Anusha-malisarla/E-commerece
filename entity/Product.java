0c937b088a4923c503a4297418388f0fc26cbe36
Anusha-malisarla <22pa5a0410@vishnu.edu.in>
2025-06-04 19:26:08 +0530

    Product.java

---
+++ entity/Product.java
@@ -0,0 +1,17 @@
+package com.example.ecommerce.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+    private String name;
+    private String description;
+    private Double price;
+    private String category;
+    private Integer stockQuantity;
+
+    // Getters & Setters
+}
